c59e61ca7fd4fe86b284ad6ed4bf47f6acd38cb9
JoZe <jose8aletor@gmail.com>
2022-09-10 13:29:31 +0000

    removeold stuff

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!bvh(0i8!4f%5@0az1&ag+6e!pnrb1wdfyw=)uc683o-6ss-(e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['azzzarel.pythonanywhere.com']
 
 
 # Application definition
@@ -118,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR/ 'static')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
